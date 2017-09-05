ea92eb98c7a5c8c6c0f0457533cc9862c803e460
dongkc <dongkechang@gmail.com>
2017-09-05 17:18:28 +0800

    updated

---
+++ .custom.el
@@ -0,0 +1,5 @@
+(setq package-archives
+    '(("melpa"   . "http://elpa.emacs-china.org/melpa/")
+      ("gnu"     . "http://elpa.emacs-china.org/gnu/")))
+(color-theme-sanityinc-solarized-light)
+(set-default-font "Source Code Pro Bold")
