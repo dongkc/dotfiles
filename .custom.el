9a90a25a8cc9055bc2c09be2a182f1ecc82f87d9
dongkc <dongkechang@foxmail.com>
2018-11-01 07:31:40 -0700

    set mobile encryption variable

--- .custom.el
+++ .custom.el
@@ -9,9 +9,11 @@
 
 ;; org mode setup
 (load (concat user-emacs-directory "org-mode.el"))
-(setq org-mobile-directory "~/git/nutstore/mobile")
+(setq org-mobile-directory "~/git/dav/mobile")
 ;; (setq org-mobile-inbox-for-pull (concat org-directory "/index.org"))
 (setq org-mobile-inbox-for-pull "~/git/org/index.org")
+(setq org-mobile-use-encryption t)
+(setq org-mobile-encryption-password "xxxxxx")
 
 ;;treate my right option as control in my macbook
 (setq mac-right-option-modifier 'control)
